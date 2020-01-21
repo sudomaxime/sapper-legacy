afc5d14df8e835189e47f27cde4e4be2a632dcc1
sudomaxime <maxime@agencesudo.ca>
2020-01-21 10:48:29 -0500

    First commit

---
+++ rollup.config.js
@@ -0,0 +1,124 @@
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import svelte from 'rollup-plugin-svelte';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import pkg from './package.json';
+
+import preprocess from 'svelte-preprocess';
+
+/**
+ * Include this module if you want to import .ts files inside .js or .svelte files
+ * Also check down below to uncomment the function calls.
+ */
+// import typescript from 'rollup-plugin-typescript2';
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
+
+export default {
+	client: {
+		input: config.client.input(),
+		output: config.client.output(),
+		plugins: [
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			svelte({
+				dev,
+				hydratable: true,
+				emitCss: true,
+				preprocess: preprocess(),
+			}),
+			resolve({
+				browser: true,
+				dedupe
+			}),
+			commonjs(),
+
+			/**
+			 * Uncomment this if you wish to use typescript outsite .svelte files
+			 */
+			// typescript({
+			// 	check: false
+			// }),
+
+			legacy && babel({
+				extensions: ['.js', '.mjs', '.html', '.svelte', '.ts'],
+				runtimeHelpers: true,
+				exclude: ['node_modules/@babel/**'],
+				presets: [
+					['@babel/preset-env', {
+						targets: '> 0.25%, not dead'
+					}]
+				],
+				plugins: [
+					'@babel/plugin-syntax-dynamic-import',
+					['@babel/plugin-transform-runtime', {
+						useESModules: true
+					}]
+				]
+			}),
+
+			!dev && terser({
+				module: true
+			})
+		],
+
+		onwarn,
+	},
+
+	server: {
+		input: config.server.input(),
+		output: config.server.output(),
+		plugins: [
+			replace({
+				'process.browser': false,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			svelte({
+				generate: 'ssr',
+				dev,
+				preprocess: preprocess(),
+			}),
+			resolve({
+				dedupe
+			}),
+			commonjs(),
+			/**
+			 * Uncomment this if you wish to use typescript outsite .svelte files
+			 */
+			// typescript({
+			// 	check: false
+			// }),
+		],
+		external: Object.keys(pkg.dependencies).concat(
+			require('module').builtinModules || Object.keys(process.binding('natives'))
+		),
+
+		onwarn,
+	},
+
+	serviceworker: {
+		input: config.serviceworker.input(),
+		output: config.serviceworker.output(),
+		plugins: [
+			resolve(),
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			commonjs(),
+			!dev && terser()
+		],
+
+		onwarn,
+	}
+};
---
+++ src/routes/index.svelte
@@ -0,0 +1,63 @@
+<script context="module">
+	import axios from 'axios';
+
+  export async function preload (page) {
+		const { data } = await axios.get('https://api.jsonbin.io/b/5e20fb265df640720836b4c7');
+    return { data }
+	}
+</script>
+
+<style type="scss">
+
+	$red: red;
+
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+		color: $red;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<script>
+	export let data; 
+</script>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+ 
+<h1>{data.message}</h1>
+
+<figure>
+	<img alt='Borat' src='great-success.png'>
+	<figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
