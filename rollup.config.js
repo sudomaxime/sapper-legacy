1dcfb57ff1f2bd343b5934e970cd95b1900ec5fb
sudomaxime <maxime@agencesudo.ca>
2020-02-24 07:50:11 -0500

    Change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,22 @@
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
+import alias from '@rollup/plugin-alias';
 import svelte from 'rollup-plugin-svelte';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
+import preprocess from 'svelte-preprocess'
+import typescript from 'rollup-plugin-typescript2';
 
-import preprocess from 'svelte-preprocess';
 
 /**
- * Include this module if you want to import .ts files inside .js or .svelte files
- * Also check down below to uncomment the function calls.
+ * Switch to ON if you wish to use typescript in
+ * your project, do not forget to confiure your
+ * tsconfig.json file accordingly.
  */
-// import typescript from 'rollup-plugin-typescript2';
+const useTypescript = false;
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
@@ -22,20 +25,40 @@ const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
 const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
 
+const aliasEntries = [
+	{ find: "styles", replacement: __dirname + 'src/styles/' },
+	{ find: "components", replacement: __dirname + '/src/components/' },
+	{ find: "pages", replacement: __dirname + '/src/pages/' },
+	{ find: "store", replacement: __dirname + '/src/store/' },
+	{ find: "locales", replacement: __dirname + '/src/locales/' }
+]
+
+const customResolver = resolve({
+  extensions: ['.js', '.scss', '.css', '.ts', '.svelte']
+});
+
 export default {
 	client: {
 		input: config.client.input(),
 		output: config.client.output(),
 		plugins: [
+			alias({
+				entries: aliasEntries,
+				customResolver
+			}),
 			replace({
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
 			svelte({
+				preprocess: preprocess({
+					typescript: {
+						transpileOnly: true,
+					}
+				}),
 				dev,
 				hydratable: true,
 				emitCss: true,
-				preprocess: preprocess(),
 			}),
 			resolve({
 				browser: true,
@@ -43,12 +66,9 @@ export default {
 			}),
 			commonjs(),
 
-			/**
-			 * Uncomment this if you wish to use typescript outsite .svelte files
-			 */
-			// typescript({
-			// 	check: false
-			// }),
+			useTypescript && typescript({
+				check: false
+			}),
 
 			legacy && babel({
 				extensions: ['.js', '.mjs', '.html', '.svelte', '.ts'],
@@ -79,25 +99,30 @@ export default {
 		input: config.server.input(),
 		output: config.server.output(),
 		plugins: [
+			alias({
+				entries: aliasEntries,
+				customResolver
+			}),
 			replace({
 				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
 			svelte({
+				preprocess: preprocess({
+					typescript: {
+						transpileOnly: true,
+					}
+				}),
 				generate: 'ssr',
-				dev,
-				preprocess: preprocess(),
+				dev
 			}),
 			resolve({
 				dedupe
 			}),
 			commonjs(),
-			/**
-			 * Uncomment this if you wish to use typescript outsite .svelte files
-			 */
-			// typescript({
-			// 	check: false
-			// }),
+			useTypescript && typescript({
+				check: false
+			}),
 		],
 		external: Object.keys(pkg.dependencies).concat(
 			require('module').builtinModules || Object.keys(process.binding('natives'))
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,4 +1,7 @@
 <style type="scss" global>
+	/**
+		Aliases like styles/global do not work yet in style tags. Coming soon !
+	*/
 	@import "../styles/global.scss";
 	/**
 		Do not add CSS here, if you have global modules to put in the layout, 
